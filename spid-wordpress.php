e01320a15623f9afa4cac79828cfbec2ec70320d
Paolo Greppi <paolo.greppi@libpf.com>
2018-07-23 11:04:15 +0200

    fix autoloading

--- spid-wordpress.php
+++ spid-wordpress.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-require_once("../vendor/autoload.php");
+require_once("vendor/autoload.php");
 require_once dirname(__FILE__) . '/src/SpidWordPress.php';
 
 SpidWordPress::getInstance();
