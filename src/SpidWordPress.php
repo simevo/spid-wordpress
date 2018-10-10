db32cb42084d225a00a30f7d55c74c885cdc3e6f
Giulio Gatto <giuliogatto@gmail.com>
2018-10-10 18:03:35 +0200

    attributes are extracted from WP options and inserted into settings

--- src/SpidWordPress.php
+++ src/SpidWordPress.php
@@ -9,6 +9,8 @@ class SpidWordPress
 
     private $auth = null;
 
+    public $options;
+
     public static function getInstance()
     {
         if (! isset(self::$instance)) {
@@ -23,6 +25,10 @@ class SpidWordPress
         $base = get_home_url();
         $home = "wp-content/plugins/spid-wordpress/docker";
 
+        $this->options = $this->getOptions();
+
+        $sp_attributeconsumingservice = $this->findActiveAttributes($this->options);
+        
         $settings = [
             'sp_entityid' => $base,
             'sp_key_file' => "$home/wp.key",
@@ -34,9 +40,7 @@ class SpidWordPress
             'sp_org_name' => 'test',
             'sp_org_display_name' => 'Test',
             'idp_metadata_folder' => "$home/idp_metadata/",
-            'sp_attributeconsumingservice' => [
-                ["name", "familyName", "fiscalNumber", "email"]
-                ]
+            'sp_attributeconsumingservice' => [$sp_attributeconsumingservice]
             ];
         $this->auth = new Italia\Spid\Sp($settings);
 
@@ -48,19 +52,21 @@ class SpidWordPress
         add_filter('authenticate', array( $this, 'filterAuthenticate' ), 21, 3);
 
         $this->define_admin_hooks();
+
     }
 
     public function filterLoginMessage($message)
     {
-        $sp_sso = get_option('sp_sso', 'spid');
-        $sp_idp = get_option('sp_idp', 'testenv2');
+        $options = $this->options;
+ 
         $query_args  = array(
-            'sso' => $sp_sso,
-            'idp' => $sp_idp
+            'sso' => $options['sp_sso'],
+            'idp' => $options['sp_idp']
         );
         echo '<div><a class="button" href="' .
             esc_url(add_query_arg($query_args, wp_login_url())) .
             '">Accedi con SPID usando testenv2 come IdP</a></div>';
+
     }
 
     public function filterAuthenticate($user, $username, $password)
@@ -112,5 +118,41 @@ class SpidWordPress
 
     private function define_admin_hooks() {
         $plugin_admin = new SpidWordpressAdmin();   
+    }
+
+    public function getOptions(){
+        $options = [];
+        
+        $options['sp_org_name'] = get_option('sp_org_name');
+        $options['sp_sso'] = get_option('sp_sso', 'spid');
+        $options['sp_idp'] = get_option('sp_idp', 'testenv2');
+        $options['sp_livello'] = get_option('sp_livello');
+        $options['sp_attributes']['sp_spidCode'] = get_option('sp_spidCode');
+        $options['sp_attributes']['sp_name'] = get_option('sp_name');
+        $options['sp_attributes']['sp_familyName'] = get_option('sp_familyName');
+        $options['sp_attributes']['sp_placeOfBirth'] = get_option('sp_placeOfBirth');
+        $options['sp_attributes']['sp_countyOfBirth'] = get_option('sp_countyOfBirth');
+        $options['sp_attributes']['sp_dateOfBirth'] = get_option('sp_dateOfBirth');
+        $options['sp_attributes']['sp_gender'] = get_option('sp_gender');
+        $options['sp_attributes']['sp_companyName'] = get_option('sp_companyName');
+        $options['sp_attributes']['sp_registeredOffice'] = get_option('sp_registeredOffice');
+        $options['sp_attributes']['sp_fiscalNumber'] = get_option('sp_fiscalNumber');
+        $options['sp_attributes']['sp_ivaCode'] = get_option('sp_ivaCode');
+        $options['sp_attributes']['sp_idCard'] = get_option('sp_idCard');
+        $options['sp_attributes']['sp_mobilePhone'] = get_option('sp_mobilePhone');
+        $options['sp_attributes']['sp_email'] = get_option('sp_email');
+        $options['sp_attributes']['sp_address'] = get_option('sp_address');
+        $options['sp_attributes']['sp_expirationDate'] = get_option('sp_expirationDate');
+        $options['sp_attributes']['sp_digitalAddress'] = get_option('sp_digitalAddress');
+        
+        return $options;
+    }
+
+    public function findActiveAttributes($options){
+        $activeAttributes = [];
+        foreach ($options['sp_attributes'] as $key => $value) {
+            if($value=='on')$activeAttributes[] = $key;
+        }
+        return $activeAttributes;
     }
 }
