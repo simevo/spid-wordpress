c592317acf454c37b8fd92234077850bb145fddc
Giulio Gatto <giuliogatto@gmail.com>
2018-10-10 15:49:21 +0200

    added attributes to settings

--- src/admin/views/settings_form.php
+++ src/admin/views/settings_form.php
@@ -3,18 +3,51 @@
 defined('WPINC') or die;
 
 if (array_key_exists('save_options', $_POST)) {
-	echo "<div id='setting-error-settings-updated' class='updated settings-error notice is-dismissable'><strong>Opzioni salvate</strong></div>";
+    echo "<div id='setting-error-settings-updated' class='updated settings-error notice is-dismissable'><strong>Opzioni salvate</strong></div>";
     update_option('sp_org_name', $_POST['sp_org_name']);
     update_option('sp_sso', $_POST['sp_sso']);
     update_option('sp_idp', $_POST['sp_idp']);
     update_option('sp_livello', $_POST['sp_livello']);
+    update_option('sp_spidCode', $_POST['sp_spidCode']);
+    update_option('sp_name', $_POST['sp_name']);
+    update_option('sp_familyName', $_POST['sp_familyName']);
+    update_option('sp_placeOfBirth', $_POST['sp_placeOfBirth']);
+    update_option('sp_countyOfBirth', $_POST['sp_countyOfBirth']);
+    update_option('sp_dateOfBirth', $_POST['sp_dateOfBirth']);
+    update_option('sp_gender', $_POST['sp_gender']);
+    update_option('sp_companyName', $_POST['sp_companyName']);
+    update_option('sp_registeredOffice', $_POST['sp_registeredOffice']);
+    update_option('sp_fiscalNumber', $_POST['sp_fiscalNumber']);
+    update_option('sp_ivaCode', $_POST['sp_ivaCode']);
+    update_option('sp_idCard', $_POST['sp_idCard']);
+    update_option('sp_mobilePhone', $_POST['sp_mobilePhone']);
+    update_option('sp_email', $_POST['sp_email']);
+    update_option('sp_address', $_POST['sp_address']);
+    update_option('sp_expirationDate', $_POST['sp_expirationDate']);
+    update_option('sp_digitalAddress', $_POST['sp_digitalAddress']);
 }
 
-$sp_org_name = get_option('sp_org_name', 'nessuno');
-$sp_sso = get_option('sp_sso', 'spid');
-$sp_idp = get_option('sp_idp', 'testenv2');
-$sp_livello = get_option('sp_livello', '1');
-
+$sp_org_name         = get_option('sp_org_name', 'nessuno');
+$sp_sso              = get_option('sp_sso', 'spid');
+$sp_idp              = get_option('sp_idp', 'testenv2');
+$sp_livello          = get_option('sp_livello', '1');
+$sp_spidCode         = get_option('sp_spidCode', '');
+$sp_name             = get_option('sp_name', '');
+$sp_familyName       = get_option('sp_familyName', '');
+$sp_placeOfBirth     = get_option('sp_placeOfBirth', '');
+$sp_countyOfBirth    = get_option('sp_countyOfBirth', '');
+$sp_dateOfBirth      = get_option('sp_dateOfBirth', '');
+$sp_gender           = get_option('sp_gender', '');
+$sp_companyName      = get_option('sp_companyName', '');
+$sp_registeredOffice = get_option('sp_registeredOffice', '');
+$sp_fiscalNumber     = get_option('sp_fiscalNumber', '');
+$sp_ivaCode          = get_option('sp_ivaCode', '');
+$sp_idCard           = get_option('sp_idCard', '');
+$sp_mobilePhone      = get_option('sp_mobilePhone', '');
+$sp_email            = get_option('sp_email', '');
+$sp_address          = get_option('sp_address', '');
+$sp_expirationDate   = get_option('sp_expirationDate', '');
+$sp_digitalAddress   = get_option('sp_digitalAddress', '');
 ?>
 <div class="wrap">
 
@@ -43,6 +76,141 @@ $sp_livello = get_option('sp_livello', '1');
 					  <option value="1" <?php if($sp_livello==1)echo "selected";?>>1</option>
 					  <option value="2" <?php if($sp_livello==2)echo "selected";?>>2</option>
 					</select>
+				</td>
+			</tr>
+			<tr>
+				<th>Attributi</th>
+				<td>
+					<fieldset>
+				    	<legend class="screen-reader-text">
+				    		<span>Attributi</span>
+				    	</legend>
+				    	<table>
+				    		<tr>
+				    			<td>
+									<label for="sp_spidCode">
+										<input name="sp_spidCode" type="checkbox" <?php if($sp_spidCode)echo"checked";?> id="sp_spidCode">
+										Codice identificativo 
+									</label>
+								</td>
+								<td>
+									<label for="sp_name">
+										<input name="sp_name" type="checkbox" <?php if($sp_name)echo"checked";?>  id="sp_name">
+										Nome
+									</label>
+								</td>
+							</tr>
+							<tr>
+				    			<td>
+									<label for="sp_familyName">
+										<input name="sp_familyName" type="checkbox" <?php if($sp_familyName)echo"checked";?>  id="sp_familyName">
+										Cognome
+									</label>
+								</td>
+								<td>
+									<label for="sp_placeOfBirth">
+										<input name="sp_placeOfBirth" type="checkbox" <?php if($sp_placeOfBirth)echo"checked";?>  id="sp_placeOfBirth">
+										Luogo di nascita
+									</label>
+								</td>
+							</tr>
+							<tr>
+				    			<td>
+									<label for="sp_countyOfBirth">
+										<input name="sp_countyOfBirth" type="checkbox" <?php if($sp_countyOfBirth)echo"checked";?>  id="sp_countyOfBirth">
+										Provincia di nascita
+									</label>
+								</td>
+								<td>
+									<label for="sp_dateOfBirth">
+										<input name="sp_dateOfBirth" type="checkbox" <?php if($sp_dateOfBirth)echo"checked";?>  id="sp_dateOfBirth">
+										Data di nascita
+									</label>
+								</td>
+							</tr>
+							<tr>
+				    			<td>
+									<label for="sp_gender">
+										<input name="sp_gender" type="checkbox" <?php if($sp_gender)echo"checked";?>  id="sp_gender">
+										Sesso
+									</label>
+								</td>
+								<td>
+									<label for="sp_companyName">
+										<input name="sp_companyName" type="checkbox" <?php if($sp_companyName)echo"checked";?>  id="sp_companyName">
+										Ragione o denominazione sociale 
+									</label>
+								</td>
+							</tr>
+							<tr>
+				    			<td>
+									<label for="sp_registeredOffice">
+										<input name="sp_registeredOffice" type="checkbox" <?php if($sp_registeredOffice)echo"checked";?>  id="sp_registeredOffice">
+										Sede legale
+									</label>
+								</td>
+								<td>
+									<label for="sp_fiscalNumber">
+										<input name="sp_fiscalNumber" type="checkbox" <?php if($sp_fiscalNumber)echo"checked";?>  id="sp_fiscalNumber">
+										Codice fiscale
+									</label>
+								</td>
+							</tr>
+							<tr>
+				    			<td>
+									<label for="sp_ivaCode">
+										<input name="sp_ivaCode" type="checkbox" <?php if($sp_ivaCode)echo"checked";?>  id="sp_ivaCode">
+										Partita IVA
+									</label>
+								</td>
+								<td>
+									<label for="sp_idCard">
+										<input name="sp_idCard" type="checkbox" <?php if($sp_idCard)echo"checked";?>  id="sp_idCard">
+										Documento d'identità
+									</label>
+								</td>
+							</tr>
+							<tr>
+				    			<td>
+									<label for="sp_mobilePhone">
+										<input name="sp_mobilePhone" type="checkbox" <?php if($sp_mobilePhone)echo"checked";?>  id="sp_mobilePhone">
+										Numero di telefono mobile 
+									</label>
+								</td>
+								<td>
+									<label for="sp_email">
+										<input name="sp_email" type="checkbox" <?php if($sp_email)echo"checked";?>  id="sp_email">
+										Indirizzo di posta elettronica 
+									</label>
+								</td>
+							</tr>
+							<tr>
+				    			<td>
+									<label for="sp_address">
+										<input name="sp_address" type="checkbox" <?php if($sp_address)echo"checked";?>  id="sp_address">
+										Domicilio fisico
+									</label>
+								</td>
+								<td>
+									<label for="sp_expirationDate">
+										<input name="sp_expirationDate" type="checkbox" <?php if($sp_expirationDate)echo"checked";?>  id="sp_expirationDate">
+										Data di scadenza identità
+									</label>
+								</td>
+							</tr>
+							<tr>
+				    			<td>
+									<label for="sp_digitalAddress">
+										<input name="sp_digitalAddress" type="checkbox" <?php if($sp_digitalAddress)echo"checked";?>  id="sp_digitalAddress">
+										Domicilio digitale
+									</label>
+								</td>
+								<td>
+								</td>
+							</tr>
+						</table>
+						<p class="description">(Legenda)</p>
+					</fieldset> 
 				</td>
 			</tr>
 		</table>
